997f24e1bfd0c8a3c4e03a76cdfb1a74257b23b4
Suhail Saqan <suhailsaqan@users.noreply.github.com>
2022-06-17 12:49:27 -0700

    satsoverflow client

---
+++ client/site.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  title: "satsoverflow",
+  description: "satsoverflow client",
+  siteUrl: "",
+  googleAnalytic: "UA-149699646-1",
+  author: {
+    email: "suhail.saqan@gmail.com",
+    name: "Suhail Saqan",
+  },
+  lang: "en",
+};
