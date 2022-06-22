0173005873dd949a0da53cedb0f8bfc06312ba54
austinkelsay <austinkelsay@yahoo.com>
2022-06-22 16:55:05 -0500

    Hardcoding for now

--- client/util/fetcher.js
+++ client/util/fetcher.js
@@ -3,7 +3,8 @@ import axios from 'axios'
 const baseURL =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:8080/api'
-    : `https://${process.env.SITE_NAME}/api`
+    // : `https://${process.env.SITE_NAME}/api`
+    : 'https://satsoverflow.heroku.app/api'
 
 const publicFetch = axios.create({
   baseURL
